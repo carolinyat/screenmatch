f4c056a8c385209280f12280ac1d435707492444
Caroliny Abreu <88946949+carolinyat@users.noreply.github.com>
2026-02-04 11:29:57 -0300

    Add files via upload

---
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,49 @@
+// src > br.com.alura.screenmatch.modelos > Serie.java
+
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    // sobrescrita de métodos
+    // o "super" refere-se à superclasse
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodiosPorTemporada * minutosPorEpisodio;
+    }
+}
